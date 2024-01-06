5df11d76c2bb4a550b0104cc5db2f2086d355fc3
Byeongkuk Oh <lunamundus1007@gmail.com>
2024-01-06 18:43:02 +0900

    #6.0 Introduction

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,16 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+
+  console.log("call an api");
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"Continue"} />
+      <h1>Welcome!</h1>
+      <h2>{counter}</h2>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
