07b884a992e1426470744ebad8ca5eee50ff802e
Byeongkuk Oh <lunamundus1007@gmail.com>
2024-01-14 01:36:40 +0900

    #7.2 Coin Tracker - Code Challenge

--- coin-tracker-practice/src/App.js
+++ coin-tracker-practice/src/App.js
@@ -3,6 +3,9 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [getCoinValue, setGetCoinValue] = useState(0);
+  const [getCoin, setGetCoin] = useState(0);
 
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -13,20 +16,48 @@ function App() {
       });
   }, []);
 
+  const onChange = (event) => {
+    setMoney(event.target.value);
+  };
+
+  const onClick = () => {
+    setGetCoin((money / getCoinValue).toFixed(2));
+  };
+
+  const onSelect = (event) => {
+    setGetCoinValue(event.target.value);
+  };
+
   return (
     <div>
       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
+        <select onChange={onSelect}>
+          <option key={-1}>Select Coin</option>
           {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}): $ {coin.quotes.USD.price} USD
+            <option key={coin.id} value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}): $ {coin.quotes.USD.price.toFixed(2)}{" "}
+              USD
             </option>
           ))}
         </select>
       )}
+
+      <hr />
+
+      <div>
+        <input
+          value={money}
+          placeholder="Write your money...(USD)"
+          type="number"
+          onChange={onChange}
+        />
+        <button onClick={onClick}>Convert</button>
+      </div>
+
+      <div>{getCoin}</div>
     </div>
   );
 }
